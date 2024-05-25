b486caf636a681c2c8969a2b3e2fc21e4ed62a9e
shou1027 <longword871@gmail.com>
2024-05-26 04:20:06 +0900

    ページナビゲーション追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { navigationMenus } from './constants/navigationMenus';
+
 function App() {
   return (
-    <main className="mx-auto mt-10 max-w-xl">
-      <h1 className="text-center text-4xl">Todoアプリ</h1>
-    </main>
+    <div>
+      <div className="min-h-screen"></div>
+      <nav className="sticky bottom-0 z-10 border-t border-secondary-border bg-white lg:hidden">
+        <ul className="grid grid-cols-4">
+          {navigationMenus.map((menu, index) => (
+            <li key={index}>
+              <a
+                href={menu.link}
+                className="flex flex-col items-center justify-center gap-1 py-1.5 text-xs hover:bg-secondary-hover"
+              >
+                <menu.icon className="size-[24px] stroke-2" />
+                {menu.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 }
 
---
+++ src/constants/navigationMenus.ts
@@ -0,0 +1,24 @@
+import { Bell, Camera, Home, User } from 'lucide-react';
+
+export const navigationMenus = [
+  {
+    link: '/',
+    label: 'ホーム',
+    icon: Home,
+  },
+  {
+    link: '/',
+    label: 'お知らせ',
+    icon: Bell,
+  },
+  {
+    link: '/',
+    label: '出品',
+    icon: Camera,
+  },
+  {
+    link: '/',
+    label: 'マイページ',
+    icon: User,
+  },
+];
