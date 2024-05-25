7123804bb6d79e9a020665d2f166158016d0ce89
shou1027 <longword871@gmail.com>
2024-05-26 04:55:02 +0900

    カテゴリを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { Categories } from './components';
 import { PageNavigation } from './layouts/PageNavigation';
 
-function App() {
+export default function App() {
   return (
     <div>
+      <div className="sticky top-0 z-10 bg-white">
+        <Categories />
+      </div>
       <div className="min-h-screen"></div>
       <PageNavigation />
     </div>
   );
 }
-
-export default App;
