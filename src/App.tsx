b091d6223940d7e681e1ed902f63e98d1b58c4e2
shou1027 <longword871@gmail.com>
2024-05-27 23:35:05 +0900

    ページヘッダ追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Categories } from './components';
+import { PageHeader } from './layouts';
 import { PageNavigation } from './layouts/PageNavigation';
 
 export default function App() {
   return (
     <div>
       <div className="sticky top-0 z-10 bg-white">
+        <PageHeader />
         <Categories />
       </div>
       <div className="min-h-screen"></div>
